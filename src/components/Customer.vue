bffba52dc1d2c10f5c4947ff89662888b4e258c0
SurajPawar15-smart <pawarsuraj614@gmail.com>
2024-03-31 14:29:15 +0530

    first commit

---
+++ src/components/Customer.vue
@@ -0,0 +1,17 @@
+<template>
+    <h2>Customer</h2>
+</template>
+
+<script>
+export default{
+    name:'Customer',
+     props: {
+    msg: String
+  }
+};
+
+</script>
+
+<style scoped>
+
+</style>
